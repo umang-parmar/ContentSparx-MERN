0beb1575d20429f6ad0fe2e8bdc5f499ae285ef8
umang-parmar <umang2502parmar@gmail.com>
2026-03-15 19:06:02 +0530

    in return add date,image,new comment, comment foem created

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router";
-import { assets, blog_data } from "../assets/assets";
+import { assets, blog_data, comments_data } from "../assets/assets";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Moment from "moment";
@@ -10,16 +10,27 @@ const Blog = () => {
   const { id } = useParams();
 
   const [data, setData] = useState(null);
+  const [comments, setComments] = useState([]);
 
   const fetchBlogData = async () => {
     const data = blog_data.find((item) => item._id === id);
     setData(data); //set the the according to the id backend and client
   };
 
+  const fetchComments = async (e) => {
+    setComments(comments_data);
+  };
+
+   const addComment = async (e) => {
+    e.preventDefault();
+  };
+
   useEffect(() => {
-    fetchBlogData(); // for the ui automatically change
+    (fetchBlogData(), // for the ui automatically change
+      fetchComments());
   }, []);
 
+  //we will display ui wrote into the return not logic easy
   return data ? (
     <div className="relative">
       <img
@@ -27,7 +38,7 @@ const Blog = () => {
         alt=""
         className="absolute -top-50 -z-1 opacity-50"
       />
-
+      {/* its a logo for the header  */}
       <Navbar />
 
       <div className="text-center mt-20 text-gray-600 ">
@@ -40,10 +51,65 @@ const Blog = () => {
         <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
         <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-indigo-500/35 bg-indigo-500/5 font-medium text-indigo-500">
           Michael Brown
-        </p>{" "}
+        </p>
       </div>
 
-      <div></div>
+      <div className="mx-4 max-w-5xl md:mx-auto my-10 mt-6 ">
+        <img src={data.image} alt="" className="rounded-3xl mb-5" />
+        {/* rich-text class & description styling take into the index.css file  */}
+        <div
+          className="rich-text max-w-3xl mx-auto"
+          dangerouslySetInnerHTML={{ __html: data.description }}
+        ></div>
+
+        {/* Comment Section  */}
+        <div className="mt-14 mb-10 max-w-3xl mx-auto">
+          <p className="font-semibold mb-4">Comments- ({comments.length})</p>
+          <div className="flex flex-col gap-4">
+            {comments.map((item, index) => (
+              <div
+                key={index}
+                className="relative bg-blue-50 border border-blue-100 max-w-xl p-4 rounded text-gray-600"
+              >
+                <div className="flex items-center gap-2 mb-2">
+                  <img src={assets.user_icon} alt="" className="w-6" />
+                  <p className="font-medium">{item.name}</p>
+                </div>
+                <p className="text-sm max-w-md ml-8">{item.content}</p>
+                <div className="absolute right-4 bottom-3 flex items-center gap-2 text-xs">
+                  {Moment(item.createdAt).fromNow()}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* Add Comment Section  */}
+        <div className="max-4-3xl mx-auto">
+          <p className="font-semibold mb-4">Add Your Comment </p>
+          <form
+            onSubmit={addComment}
+            className="flex flex-col items-start gap-4 max-w-lg"
+          >
+            <input
+              type="text"
+              placeholder="Name"
+              required
+              className="w-full p-2 border border-gray-300 rounded outline-none"
+            />
+            <textarea
+              className="w-full p-2 border border-gray-300 rounded outline-none h-48"
+              placeholder="Comment"
+              required
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white rounded p-2 px-8 hover:scale-105 hover:bg-blue-600 transition-all cursor-pointer"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   ) : (
     <div>Loading...</div>
