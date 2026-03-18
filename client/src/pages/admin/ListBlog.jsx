df71e301c28d9452d00502b48a7088b65ba90b76
umang-parmar <umang2502parmar@gmail.com>
2026-03-18 08:27:41 +0530

    in dashboard listblog Done

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { blog_data } from '../../assets/assets';
+import BlogTableitem from './BlogTableitem';
 
 const ListBlog = () => {
+
+const [blogs, setBlogs] = useState([]);
+
+const fetchBlogs = async ()=>{
+  setBlogs(blog_data)
+}
+
+useEffect(()=>{
+  fetchBlogs();
+},[])
+
   return (
-    <div>ListBlog</div>
+    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50'>
+      <h1>All Blogs</h1>
+        {/* copy from dashboard for table create  */}
+        <div className='relative h-4/5 max-w-4xl mt-4 overflow-x-auto shadow rounded-lg scrollbar-hide bg-white'>
+          <table className='w-full text-sm text-gray-500'>
+            <thead className='text-xs text-gray-600 text-left uppercase'>
+              <tr>
+                <th scope="col" className='px-2 py-4 xl:px-6'> # </th>
+                <th scope="col" className='px-2 py-4'> Blog Title </th>
+                <th scope="col" className='px-2 py-4 max-sm:hidden'> Date </th>
+                <th scope="col" className='px-2 py-4 max-sm:hidden'> Status </th>
+                <th scope="col" className='px-2 py-4'> Actions </th>
+              </tr>
+            </thead>
+            <tbody>
+              {blogs.map((blog,index)=>{
+                return <BlogTableitem key={blog._id} blog={blog} fetchBlogs={fetchBlogs} index={index + 1}/>
+              })}
+            </tbody>
+          </table>
+        </div> 
+    </div>
   )
 }
 
